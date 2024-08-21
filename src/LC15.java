7540adc817974818c8b865aaaf312f262b4dc9a7
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-08-21 17:16:55 +0530

    Enhanced readability of Lc.3 Sum

--- src/LC15.java
+++ src/LC15.java
@@ -12,26 +12,34 @@ public class LC15 {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int l = i + 1;
-            int r = nums.length - 1;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;//skip duplicate for first pointer
+            int l = i + 1;//pointer l
+            int r = nums.length - 1;//pointer r
 
             while (l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
 
-                if (sum == 0) {
+                if (sum == 0)
+                {
                     res.add(Arrays.asList(nums[i], nums[l], nums[r]));
 
                     // Skip duplicates for the left pointer
-                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[l] == nums[l + 1])
+                        l++;//run until no duplicate
+
                     // Skip duplicates for the right pointer
-                    while (l < r && nums[r] == nums[r - 1]) r--;
+                    while (l < r && nums[r] == nums[r - 1])
+                        r--;//decrease until no duplicate
 
                     l++;
                     r--;
-                } else if (sum < 0) {
+                }
+                else if (sum < 0)
+                {
                     l++;
-                } else {
+                }
+                else
+                {
                     r--;
                 }
             }
