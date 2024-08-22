518b5bcb709a7096cee928127536f73ab1d3382c
deep-priyo <priyadeepmullick@gmail.com>
2024-08-22 20:51:48 +0530

    Solves 11. Container With Most Water->Optimised Approach O(n) -> Medium

--- src/LC11.java
+++ src/LC11.java
@@ -2,7 +2,7 @@
 public class LC11 {
     public static void main(String[] args) {
         int[] arr={1,8,6,2,5,4,8,3,7};
-        System.out.println(maxArea(arr));
+        System.out.println(maxArea2(arr));
 
     }
     public static int maxArea(int[] height) {
@@ -16,6 +16,25 @@ public class LC11 {
         }
     }
     return max;
+    }
+    public static int maxArea2(int[] height) {
+        int max=0;
+        int i=0;
+        int j=height.length-1;
+        while(i<j){
+
+            int area = Math.min(height[i],height[j]) * (j - i);
+            max = Math.max(max, area);
+            if(height[i]<height[j]){
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+
+        }
+        return max;
     }
 }
 
