71029e001f495765fb15ab1170e8e8dbc63f40ec
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-08-25 12:42:15 +0530

    Solved 739. Daily Temperatures -> Stack -> Medium -> Optimised approach 0(N)

--- src/LC739.java
+++ src/LC739.java
@@ -1,10 +1,11 @@
 import java.util.Arrays;
+import java.util.Stack;
 
 //https://leetcode.com/problems/daily-temperatures/description/
 public class LC739 {
     public static void main(String[] args) {
         int[] arr={73,74,75,71,69,72,76,73};
-        System.out.println(Arrays.toString(dailyTemperatures(arr)));
+        System.out.println(Arrays.toString(dailyTemperatures2(arr)));
     }
     //brute force approach time limit exceeded O(n^2)
     public static int[] dailyTemperatures(int[] temperatures) {
@@ -18,6 +19,20 @@ public class LC739 {
                     break;
                 }
             }
+        }
+        return arr;
+    }
+    //optimized approach
+    public static int[] dailyTemperatures2(int[] temperatures) {
+        Stack<Integer> stack=new Stack<Integer>();
+        int[] arr=new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int a = stack.pop();
+                arr[a] = i - a; // Set the result for day 'a'
+            }
+            stack.push(i); // Push the current day's index onto the stack
+
         }
         return arr;
     }
