3c5da24daac6c1ace0a5d5644dc0c7705cf35ed9
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-09-14 20:16:52 +0530

    567. Permutation in String ->Sliding Window-> Medium -> 0(n+m)

--- src/LC567.java
+++ src/LC567.java
@@ -1,5 +1,5 @@
 import java.util.HashMap;
-
+//https://leetcode.com/problems/permutation-in-string/description/
 public class LC567 {
     public static void main(String[] args) {
  String s1="adc";
@@ -7,17 +7,16 @@ public class LC567 {
         System.out.println(checkInclusion(s1, s2));
     }
     public static boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false; // s1 is bigger so cannot get a permutation ever
+
         HashMap<Character, Integer> mapS1 = new HashMap<>();
         HashMap<Character, Integer> mapS2 = new HashMap<>();
+        // Create a frequency map for s1
         for(char c: s1.toCharArray()) {
-            if(mapS1.containsKey(c)) {
-                mapS1.put(c, mapS1.get(c) + 1);
-            }
-            else
-                mapS1.put(c, 1);
+            mapS1.put(c, mapS1.getOrDefault(c, 0) + 1);
         }
         int i=0;
-        int j=s1.length();
+        int j=s1.length();// the location of the second pointer = length of s1 the sub-strings should be of equal length
         while(j<=s2.length()) {
             String s=s2.substring(i,j);
             System.out.println(s);
