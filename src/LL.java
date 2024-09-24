723c431f99a43126524f19d3a4aed2e267974bbe
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-09-24 13:07:08 +0530

    Implemented Custom Linked List Class

---
+++ src/LL.java
@@ -0,0 +1,154 @@
+public class LL {
+    private Node head;
+    private Node tail;
+    private int size;
+    public LL() {
+        this.size = 0;
+    }
+    public void insertAtFirst(int val)
+    {
+        Node node = new Node(val);// I have the new node
+
+        node.next = head;
+        head = node;
+        if(tail == null)// this is the first node
+            tail = node;
+        size++;
+    }
+    public void insertAtLast(int val)
+    {
+        if(tail == null) {
+        insertAtFirst(val);
+        return;}
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+    }
+    public void insert(int value,int index)
+    {
+        if(index==0) {
+            insertAtFirst(value);
+            return;
+        }
+        if(index==size) {
+            insertAtLast(value);
+            return;
+        }
+        Node temp=head;
+        while(index>1) {
+            temp=temp.next;
+            index--;
+        }
+        Node newNode = new Node(value,temp.next);
+        temp.next = newNode;
+
+
+
+    }
+    public Node get(int index)
+    {
+        Node node = head;
+        for(int i=0;i<index;i++) {
+            node=node.next;
+        }
+        return node;
+    }
+    public int deleteLast2()
+    {
+        if(size<=1)
+        {
+            return deleteFirst();
+        }
+        Node secondLast=get(size-2);
+        int val=tail.value;
+        tail=secondLast;
+        tail.next=null;
+        return val;
+    }
+    public int deleteAtIndex(int index)
+    {
+        if(index==0) {
+            return deleteFirst();
+        }
+        if(index==size-1) {
+            return deleteLast2();
+        }
+        Node prev=get(index-1);
+        int val=prev.next.value;
+        prev.next=prev.next.next;
+        return val;
+    }public Node findValue(int data)
+    {
+        Node temp=head;
+        while(temp.next!=null) {
+            if(temp.value==data) {
+                return temp;
+            }
+            temp=temp.next;
+        }
+        return null;
+    }
+    public int deleteLast()
+    {
+        Node temp=head;
+        int val=tail.value;
+        if(head==null) {
+            System.out.println("List is empty");
+            return -1;
+        }
+        if(head==tail) {
+            head=tail=null;
+            return val ;
+        }
+        while(temp.next!=tail) {
+            temp=temp.next;
+        }
+        tail=temp;
+        tail.next=null;
+        return val;
+    }
+    public int deleteFirst()
+    {
+        int val=head.value;
+        head = head.next;
+        if(head==null)
+            tail = null;
+        size--;
+        return val;
+    }
+    public void displayHead()
+    {
+        System.out.println("Head: " + head.value);
+    }
+    public void displayTail()
+    {
+        System.out.println("Tail: " + tail.value);
+    }
+    public void getSize(){
+        System.out.println("Size: " + size);
+    }
+    public void display()
+    {
+        Node pointer = head;
+        while(pointer != null) {
+            System.out.print(pointer.value + "->");
+            pointer = pointer.next;
+        }
+        System.out.print("END\n");
+    }
+    public class Node
+    {
+        private int value;
+        private Node next;
+        public Node(int value)
+        {
+            this.value = value;
+        }
+        public Node(int value, Node next)
+        {
+            this.value = value;
+            this.next = next;
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LL linkedList = new LL();
+        linkedList.insertAtFirst(5);
+        linkedList.insertAtFirst(4);
+        linkedList.insertAtFirst(3);
+        linkedList.insertAtFirst(2);
+        linkedList.insertAtFirst(1);
+        linkedList.insert(45,2);
+        linkedList.display();
+        linkedList.displayHead();
+        linkedList.displayTail();
+        linkedList.deleteFirst();
+        linkedList.display();
+        linkedList.deleteLast();
+        linkedList.display();
+        linkedList.deleteAtIndex(2);
+        linkedList.display();
+        //linkedList.getSize();
     }
 }
