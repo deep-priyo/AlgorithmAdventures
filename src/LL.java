0515c634fba9602df8f57063d9edafeb43772964
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-09-26 18:00:23 +0530

    206. Reverse Linked List -> Easy->Recursion

--- src/LL.java
+++ src/LL.java
@@ -244,6 +244,18 @@ public class LL {
         }
         return ans;
     }
+    public void reverse(Node node)
+    {
+        if(node==tail)
+        {
+            head=tail;
+            return;
+        }
+        reverse(node.next);
+        tail.next=node;
+        tail=node;
+        tail.next=null;
+    }
 
     public static void main(String[] args) {
         LL first=new LL();
