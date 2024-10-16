f9357941457df4852ff75eb1e46f96f6af77d155
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-10-16 20:48:11 +0530

    2095. Delete the Middle Node of a Linked List-> Medium ->O(n)

--- src/LL.java
+++ src/LL.java
@@ -200,6 +200,21 @@ public class LL {
         }
         return s;
     }
+    public ListNode deleteMiddle(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+        ListNode s=head;
+        ListNode f=head;
+        ListNode prev=head;
+        while(f != null && f.next !=null)
+        {
+            prev=s;
+            s=s.next;
+            f=f.next.next;
+        }
+        prev.next=prev.next.next;
+        return head;
+    }
     public ListNode detectCycle(ListNode head) {
         int lengthCycle= hasCycle(head);
         if(lengthCycle==0)return null;
