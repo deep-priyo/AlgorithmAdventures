ae444451fc1853080a1a44a6633c3fb30a03a8eb
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-11-05 11:38:54 +0530

    2914. Minimum Number of Changes to Make Binary String Beautiful->Medium->O(n)

---
+++ src/LC2914.java
@@ -0,0 +1,11 @@
+public class LC2914 {
+    public static int minChanges(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length() - 1; i += 2) {
+            if (s.charAt(i) != s.charAt(i + 1)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
