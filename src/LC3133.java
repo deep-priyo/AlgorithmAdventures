e30622f6f682b4db50f5a9fefb4e647f82555b31
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-11-09 13:30:00 +0530

    3133. Minimum Array End->Medium->O(n * 2^b) Optimized

--- src/LC3133.java
+++ src/LC3133.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class LC3133 {
     public static void main(String[] args) {
-        System.out.println(minEnd(3,4));
+        System.out.println(minEndOptimized(3,4));
     }
     public static long minEnd(int n, int x) {
         long[] arr = new long[n];
@@ -23,23 +23,22 @@ public class LC3133 {
         //System.out.println(Arrays.toString(arr));
         return arr[n-1];
     }
-//    public static long minEnd(int n, int x) {
-//        long[] arr = new long[n];
-//        arr[0] = x;
-//        int com=x;
-//        long j;
-//        for (int i = 1; i < n; i++) {
-//            for(j=x+1;;j++)
-//            {
-//                if((arr[i-1]&j)==com)
-//                {
-//                    arr[i] = j;
-//                    break;
-//                }
-//            }
-//            x= (int) (arr[i]);
-//        }
-//        //System.out.println(Arrays.toString(arr));
-//        return arr[n-1];
-//    }
+    public static long minEndOptimized(int n, int x) {
+        long prev = x; // Start with the initial element as x
+        long current;
+        for (int i = 1; i < n; i++) {
+            // Start from prev + 1 to ensure nums[i] > nums[i - 1]
+            current = prev + 1;
+
+            // Find the smallest number greater than prev that keeps the AND result as x
+            while ((current & x) != x) {
+                current++;
+            }
+
+            prev = current; // Update prev to be the current element
+        }
+
+        return prev; // The minimum possible value of nums[n - 1]
 }
+    }
+
