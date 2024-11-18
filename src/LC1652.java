a85ba3462b1757c8e558da48bccfa7b5587d2338
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-11-18 21:38:32 +0530

    1652. Defuse the Bomb->Easy-> O(n*k)

---
+++ src/LC1652.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class LC1652 {
+    public static void main(String[] args) {
+        int[] code={5,7,1,4};
+        System.out.println(Arrays.toString(decrypt(code,3)));
+    }
+    public static int[] decrypt(int[] code, int k) {
+
+        int n=code.length;
+        int[] res =new int[n];
+        if(k==0)
+            return res;
+
+        if(k>0)
+        {
+            for(int i=0; i<n; i++)
+            {
+                int l=i+1;
+                for (int j = 1; j <= k; j++) {
+
+                res[i]+=code[l%n];
+                l++;
+                }
+            }
+        }
+        if(k<0)
+        {
+            for (int i = 0; i < n; i++) {
+                int l = i - 1;
+                for (int j = 1; j <= -k; j++) {
+                    res[i] += code[(l + n) % n];
+                    l--;
+                }
+            }
+        }
+        return res;
+    }
+
+}
