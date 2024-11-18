eb646a1686a8c54045178d9b34b27059ba835d2d
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-11-18 21:56:40 +0530

    1652. Defuse the Bomb->Easy-> O(n*k) -> Cleaner approach

--- src/LC1652.java
+++ src/LC1652.java
@@ -15,22 +15,26 @@ public class LC1652 {
         if(k>0)
         {
             for(int i=0; i<n; i++)
-            {
-                int l=i+1;
-                for (int j = 1; j <= k; j++) {
-
-                res[i]+=code[l%n];
-                l++;
-                }
-            }
+//                int l=i+1;
+//                for (int j = 1; j <= k; j++) {
+//
+//                res[i]+=code[l%n];
+//                l++;
+//                }
+                for (int j = i+1; j <= i+k; j++)
+                    res[i]+=code[j%n];
         }
         if(k<0)
         {
             for (int i = 0; i < n; i++) {
-                int l = i - 1;
-                for (int j = 1; j <= -k; j++) {
-                    res[i] += code[(l + n) % n];
-                    l--;
+//                int l = i - 1;
+//                for (int j = 1; j <= -k; j++) {
+//                    res[i] += code[(l + n) % n];
+//                    l--;
+//                }
+                for (int j = i-1; j >= i-Math.abs(k); j--)
+                {
+                    res[i] += code[(j+n )%n];
                 }
             }
         }
