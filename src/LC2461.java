03b0a462c5f0104208406fa6c09e807698d77607
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-11-19 12:15:40 +0530

    2461. Maximum Sum of Distinct Subarrays With Length K -> Medium -> O(n)

---
+++ src/LC2461.java
@@ -0,0 +1,48 @@
+import java.util.HashSet;
+
+public class LC2461 {
+    public static void main(String[] args) {
+        int[] a={1,5,4,2,9,9,9};
+        int k=3;
+        System.out.println(maximumSubarraySum(a,k));
+    }
+    public static  long maximumSubarraySum(int[] nums, int k) {
+        HashSet<Integer> set=new HashSet<>();
+        int left=1;
+        int right=k;
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<k;i++){
+            System.out.print(nums[i]+" ");
+            if(set.contains(nums[i])){
+                System.out.println("Duplicate found");
+            }
+            else {
+                set.add(nums[i]);
+                sum+=nums[i];
+            }
+            }
+        System.out.println();
+        while (right<nums.length){
+            set.remove(nums[left-1]);
+            sum-=nums[left-1];
+            if(set.contains(nums[right])){
+
+                System.out.println("Duplicate found");
+                for (int i = left; i <=right ; i++) {
+                    System.out.print(" "+nums[i]);
+                }
+                System.out.println();
+            }
+            else {
+                set.add(nums[right]);
+                sum+=nums[right];
+            }
+            max=Math.max(max,sum);
+            System.out.println(nums[left]+" "+nums[right]);
+            right++;
+            left++;
+        }
+        return max;
+    }
+}
